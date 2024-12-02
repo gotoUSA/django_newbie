f8099ac4b4267eac59d9744de570c31bf1eed108
gotoUSA <mcbg154@naver.com>
2024-12-02 21:27:49 +0900

    sixth commit

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,4 +1,5 @@
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 
 from ..models import Question
@@ -6,10 +7,19 @@ from ..models import Question
 
 def index(request):
     page = request.GET.get("page", "1")
+    kw = request.GET.get("kw", "")
     question_list = Question.objects.order_by("-create_date")
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw)
+            | Q(content__icontains=kw)
+            | Q(answer__content__icontains=kw)
+            | Q(author__username__icontains=kw)
+            | Q(answer__author__username__icontains=kw)
+        ).distinct()
     paginator = Paginator(question_list, 10)
     page_obj = paginator.get_page(page)
-    context = {"question_list": page_obj}
+    context = {"question_list": page_obj, "page": page, "kw": kw}
     return render(request, "pybo/question_list.html", context)
 
 
