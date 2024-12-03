a36f44e185b014a2ed6e43388c74105b9516a540
gotoUSA <mcbg154@naver.com>
2024-12-03 21:43:58 +0900

    seventh commit

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -18,8 +18,20 @@ def index(request):
             | Q(answer__author__username__icontains=kw)
         ).distinct()
     paginator = Paginator(question_list, 10)
+    max_index = len(paginator.page_range)
     page_obj = paginator.get_page(page)
-    context = {"question_list": page_obj, "page": page, "kw": kw}
+    current_page = int(page)
+    group_start = ((current_page - 1) // 5) * 5 + 1
+    group_end = min(group_start + 4, paginator.num_pages)
+
+    context = {
+        "question_list": page_obj,
+        "page": page,
+        "kw": kw,
+        "max_index": max_index,
+        "group_start": group_start,
+        "group_end": group_end,
+    }
     return render(request, "pybo/question_list.html", context)
 
 
